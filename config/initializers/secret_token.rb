50c07cd845184bbc03166601e8a3d7270c2910ba
Nick Gottlieb <ngottlieb@gmail.com>
2013-03-06 15:11:59 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Necreeking::Application.config.secret_token = 'f799a28f7b239f399366f4116ac46ec59cb064199472495dd506b14376d23885e7eb04d8a614b7bacb4fd80090cc6c0146581478a11ffb67bd97c9db6680b725'
