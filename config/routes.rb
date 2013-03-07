b4cefb167bfa44d13139ed91b27e311a1964eea7
Nick Gottlieb <ngottlieb@gmail.com>
2013-03-07 12:16:33 -0500

    updated reports create to work with twilio

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Necreeking::Application.routes.draw do
-  get "reports/index"
-
-  get "reports/new"
-
-  get "reports/create"
 
   authenticated :user do
     root :to => 'home#index'
   end
+  
   root :to => "home#index"
   devise_for :users
   resources :users
+  
+  resources :reports, :only => ["index", "create", "new"]
 end
