147af33f5c71c886df1a529e8d77150c1ce2a8b1
Nick Gottlieb <ngottlieb@gmail.com>
2013-03-07 14:48:08 -0500

    adjusted config so that heroku asset pipeline will work

--- config/application.rb
+++ config/application.rb
@@ -69,6 +69,9 @@ module Necreeking
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # Heroku
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
