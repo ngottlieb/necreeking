29cc54edd8773e7eb77db0c9e6a415b7e6b0e019
Nick Gottlieb <ngottlieb@gmail.com>
2013-03-07 11:37:24 -0500

    added phone to user, and some reports related stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
+    @reports = Report.all
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -2,5 +2,7 @@ class Report < ActiveRecord::Base
   attr_accessible :email, :phone, :report, :user_id
   belongs_to :user
   
+  default_scope order("created_at DESC")
+  
   
 end
