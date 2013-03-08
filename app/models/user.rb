8122fbe16a075e13949931ff044e90015017b144
Nick Gottlieb <ngottlieb@gmail.com>
2013-03-08 08:59:18 -0500

    added location to users, user forms, and user show

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :phone
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :phone, :location
   has_many :reports
   
   validates_uniqueness_of :email, :phone
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130306212755) do
+ActiveRecord::Schema.define(:version => 20130308135432) do
 
   create_table "reports", :force => true do |t|
     t.string   "phone"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20130306212755) do
     t.datetime "updated_at",                             :null => false
     t.string   "name"
     t.string   "phone"
+    t.string   "location"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
