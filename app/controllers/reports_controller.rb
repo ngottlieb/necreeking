12e1a7862f5c1249f90f656625ca9f90bcd54395
Nick Gottlieb <ngottlieb@gmail.com>
2013-08-08 16:40:32 -0400

    fixed accidental limit on reports for logged in users

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
   def index
     if current_user and current_user.regions.count > 0
       regions = current_user.regions.map { |r| r.id }
-      @reports = Report.where(region_id: regions).limit(3)
+      @reports = Report.where(region_id: regions)
     else
       @reports = Report.all
     end
