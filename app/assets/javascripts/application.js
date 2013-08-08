1597e847acf3e18e783a4713b94b699ea433515d
Nick Gottlieb <ngottlieb@gmail.com>
2013-08-08 16:16:55 -0400

    removed sorting from datatable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@
 $(document).ready(function() {
   $('.datatable').dataTable({
     "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-    "sPaginationType": "bootstrap", 
-    "aaSorting": [[ 0, "desc" ]]
+    "sPaginationType": "bootstrap",
+    "bSort": false
   });
 });
