efb477cf35e1afe274c2b491fb1d1a5bbbb62ae3
Nick Gottlieb <ngottlieb@gmail.com>
2013-08-08 16:23:16 -0400

    quick fix due to login form not showing (js)

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("input.tel").mask("(999) 999-9999");
 	
-	$("div.password.control-group").hide();
+	// $("div.password.control-group").hide();
 	
 	$("div.control-group.email input").on('change', function () {
 	  $("div.password.user_current_password").show();
